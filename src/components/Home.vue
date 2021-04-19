2f4bc0c3b828d5ef101008752783d5589d2ed609
Cecilie Mariendal <ctmariendal@gmail.com>
2021-04-19 20:43:38 +0200

    Add User component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,15 +2,22 @@
     <div>
         <h2>Home.vue</h2>
 
-        <Login />
+        <User v-slot:user="{user}">
+            <div v-if="user">
+                Logged in as {{user.uid}}
+            </div>
+            <Login v-else />
+        </User>
     </div>
 </template>
 
 <script>
+import User from './User'
 import Login from './Login'
 
 export default {
     components: {
+        User,
         Login,
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueCompositionApi from '@vue/composition-api'
 import { firestorePlugin } from 'vuefire'
 import App from './App.vue'
 
 import Home from './components/Home.vue'
 
+Vue.use(VueCompositionApi)
 Vue.use(VueRouter)
 Vue.use(firestorePlugin)
 
