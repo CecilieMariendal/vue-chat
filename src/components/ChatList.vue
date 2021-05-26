34e03e4f8c994dbdcb798f1a38c7ed73087f9e6b
Cecilie Mariendal <ctmariendal@gmail.com>
2021-05-26 21:41:20 +0200

    Add chatlist animation

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -2,7 +2,7 @@
     <div>
         <h3>Chats</h3>
         <ul>
-            <li v-for="chat of chats" :key="chat.id">
+            <li v-for="(chat, index) in chats" :key="chat.id" :style="{animationDelay: 'calc(' + index + '00ms)'}">
                 <router-link :to="{name: 'chat', params: {id: chat.id}}">{{chat.name}}</router-link>
             </li>
         </ul>
@@ -68,12 +68,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@keyframes shiftIn {
+    100% {
+        opacity: 0;
+        transform: translateY(-10px)
+    }
+}
 ul {
-    padding: 0;
+    flex: 1;
     list-style: none;
-    
+    padding: 0;
+    filter: drop-shadow(4px 4px 10px rgba(200, 200, 200, 0.3));
+
     li  {
         margin: 1rem 0;
+        padding: 1rem;
+        background-color: white;
+        overflow: hidden;
+        animation: shiftIn 0.2s reverse backwards;
     }
 }
 
